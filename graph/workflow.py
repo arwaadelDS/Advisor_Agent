1211b89af68d0bdaa9d5b7dcdf4e50fd0e5d5bea
Avinash <Avinash.Gupta@datascience.me>
2026-08-25 10:57:36 +0530

    Fixed conditional edges routing to sql_agent and rag_agent

--- graph/workflow.py
+++ graph/workflow.py
@@ -2,15 +2,15 @@ from langgraph.graph import StateGraph, START, END
 from graph.state import AdvisorState
 from agents.supervisor import supervisor_node
 from agents.search_agent import search_agent_node
-# from agents.rag_agent import rag_agent_node
-# from agents.sql_agent import sql_agent_node
+from agents.rag_agent import rag_agent_node
+from agents.sql_agent import sql_agent_node
 
 graph = StateGraph(AdvisorState)
 
 graph.add_node("supervisor", supervisor_node)
 graph.add_node("search_agent", search_agent_node)
-# graph.add_node("rag_agent", rag_agent_node)
-# graph.add_node("sql_agent", sql_agent_node)
+graph.add_node("rag_agent", rag_agent_node)
+graph.add_node("sql_agent", sql_agent_node)
 
 graph.add_edge(START, "supervisor")
 
@@ -21,15 +21,15 @@ graph.add_conditional_edges(
     "supervisor",
     route_next,
     {
-        "sql_agent": END,
-        "rag_agent": END,
+        "sql_agent": "sql_agent",
+        "rag_agent": "rag_agent",
         "search_agent": "search_agent",
         "end": END,
     }
 )
 
 graph.add_edge("search_agent", END)
-# graph.add_edge("rag_agent", END)
-# graph.add_edge("sql_agent", END)
+graph.add_edge("rag_agent", END)
+graph.add_edge("sql_agent", END)
 
 app = graph.compile()
